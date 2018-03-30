7efdabbcf09d833f96144a437f4f9d37574f6898
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-30 12:57:50 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,6 +3,7 @@ class PigLatinizer
   def piglatinize(word)
     if word =~ (/\A[aeiou]/i)
         word = word + 'way'
+    elsif word =~ (/\A[^aeiou][^aeiou]/i)
     elsif word =~ (/\A[^aeiou]/i)
       match = /\A[^aeiou]/i.match(word)
       word = match.post_match + match.to_s + 'ay'
