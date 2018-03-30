642980e52066ce9e0a5d0f66895109d6dd2f3b92
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-30 13:09:23 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,14 +4,14 @@ class PigLatinizer
 
     arr = string.split
 
-    arr.map do |word|
-      if word =~ (/\A[aeiou]/i)
-          word = word + 'way'
-      elsif word =~ (/\A[^aeiou]{1,}/i)
-        match = /\A[^aeiou]{1,}/i.match(word)
-        word = match.post_match + match.to_s + 'ay'
-      end
-      word
-    end
+  arr2 =  arr.map do |word|
+          if word =~ (/\A[aeiou]/i)
+              word = word + 'way'
+          elsif word =~ (/\A[^aeiou]{1,}/i)
+            match = /\A[^aeiou]{1,}/i.match(word)
+            word = match.post_match + match.to_s + 'ay'
+          end
+        end
+    arr2.join(" ")
   end
 end
