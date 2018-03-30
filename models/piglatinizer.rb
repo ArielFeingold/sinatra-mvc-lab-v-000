68b929aa940fa4c4e928e8b8fbfdb81c9343eb28
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-30 13:05:57 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,17 @@
 class PigLatinizer
 
-  def piglatinize(word)
-    if word =~ (/\A[aeiou]/i)
-        word = word + 'way'
-    elsif word =~ (/\A[^aeiou]{1,}/i)
-      match = /\A[^aeiou]{1,}/i.match(word)
-      word = match.post_match + match.to_s + 'ay'
+  def piglatinize(string)
+
+    arr = string.split
+
+    arr.map do |word|
+      if word =~ (/\A[aeiou]/i)
+          word = word + 'way'
+      elsif word =~ (/\A[^aeiou]{1,}/i)
+        match = /\A[^aeiou]{1,}/i.match(word)
+        word = match.post_match + match.to_s + 'ay'
+      end
+      word
     end
-    word
   end
 end
