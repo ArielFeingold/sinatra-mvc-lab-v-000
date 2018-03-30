c52e92fe5404670949695f91ae56f0de4426fa8e
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-30 12:48:02 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,12 @@
 class PigLatinizer
-binding.pry
-  if word =~ (/\A[aeiou]/i)
-      word = word + 'ay'
-  elsif word =~ (/\A[^aeiou]/i)
-    match = /\A[^aeiou]/i.match(word)
-    word = match.post_match + match.to_s + 'ay'
-  end
+
+  def piglatinizes
+    if word =~ (/\A[aeiou]/i)
+        word = word + 'ay'
+    elsif word =~ (/\A[^aeiou]/i)
+      match = /\A[^aeiou]/i.match(word)
+      word = match.post_match + match.to_s + 'ay'
+    end
     word
+  end
 end
