0260ead05008323fc2b7a60ed9190090454ba734
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-30 12:43:04 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,11 @@
 class PigLatinizer
 
-  
-  def initialize
-    if word =~ (/\A[aeiou]/i)
+  if word =~ (/\A[aeiou]/i)
       word = word + 'ay'
   elsif word =~ (/\A[^aeiou]/i)
     match = /\A[^aeiou]/i.match(word)
     word = match.post_match + match.to_s + 'ay'
   end
     word
-  end
 
 end
