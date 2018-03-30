090d7d50896d823db3d79af698a19f240b006136
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-30 12:39:36 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,14 @@
 class PigLatinizer
 
-  attr_reader :word
 
   def initialize
-  #   if @word =~ (/\A[aeiou]/i)
-  #     @word = @word + 'ay'
-  # elsif @word =~ (/\A[^aeiou]/i)
-  #   @match = /\A[^aeiou]/i.match(word)
-  #   @word = match.post_match + match.to_s + 'ay'
-  # end
-  #   word
+    if word =~ (/\A[aeiou]/i)
+      word = word + 'ay'
+  elsif word =~ (/\A[^aeiou]/i)
+    match = /\A[^aeiou]/i.match(word)
+    word = match.post_match + match.to_s + 'ay'
+  end
+    word
   end
 
 end
